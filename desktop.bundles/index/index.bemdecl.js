0bcf7d12c56542b476ceeab99e74ccd9bd93b606
adinvadim <adin.vadim@gmail.com>
2016-06-20 01:51:25 +0300

    Dist files

---
+++ desktop.bundles/index/index.bemdecl.js
@@ -0,0 +1,672 @@
+exports.blocks = [
+    {
+        "name": "page"
+    },
+    {
+        "name": "page",
+        "elems": [
+            {
+                "name": "meta"
+            }
+        ]
+    },
+    {
+        "name": "page",
+        "elems": [
+            {
+                "name": "css"
+            }
+        ]
+    },
+    {
+        "name": "page",
+        "elems": [
+            {
+                "name": "js"
+            }
+        ]
+    },
+    {
+        "name": "section"
+    },
+    {
+        "name": "section",
+        "mods": [
+            {
+                "name": "type",
+                "vals": [
+                    {
+                        "name": "main"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "header"
+    },
+    {
+        "name": "row"
+    },
+    {
+        "name": "header",
+        "elems": [
+            {
+                "name": "menu-row"
+            }
+        ]
+    },
+    {
+        "name": "sections-menu"
+    },
+    {
+        "name": "header",
+        "elems": [
+            {
+                "name": "menu"
+            }
+        ]
+    },
+    {
+        "name": "sections-menu",
+        "elems": [
+            {
+                "name": "menu-item"
+            }
+        ]
+    },
+    {
+        "name": "sections-menu",
+        "elems": [
+            {
+                "name": "menu-item",
+                "mods": [
+                    {
+                        "name": "active",
+                        "vals": [
+                            {
+                                "name": true
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "menu-item"
+    },
+    {
+        "name": "header",
+        "elems": [
+            {
+                "name": "menu-item"
+            }
+        ]
+    },
+    {
+        "name": "sections-menu",
+        "elems": [
+            {
+                "name": "menu-item",
+                "mods": [
+                    {
+                        "name": "active"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "social"
+    },
+    {
+        "name": "logo"
+    },
+    {
+        "name": "header",
+        "elems": [
+            {
+                "name": "logo"
+            }
+        ]
+    },
+    {
+        "name": "row",
+        "elems": [
+            {
+                "name": "col"
+            }
+        ]
+    },
+    {
+        "name": "row",
+        "elems": [
+            {
+                "name": "col",
+                "mods": [
+                    {
+                        "name": "sw",
+                        "vals": [
+                            {
+                                "name": 10
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "title"
+    },
+    {
+        "name": "header",
+        "elems": [
+            {
+                "name": "title"
+            }
+        ]
+    },
+    {
+        "name": "text"
+    },
+    {
+        "name": "header",
+        "elems": [
+            {
+                "name": "text"
+            }
+        ]
+    },
+    {
+        "name": "button"
+    },
+    {
+        "name": "button",
+        "mods": [
+            {
+                "name": "view",
+                "vals": [
+                    {
+                        "name": "second"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "button",
+        "mods": [
+            {
+                "name": "size",
+                "vals": [
+                    {
+                        "name": "l"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "header",
+        "elems": [
+            {
+                "name": "button"
+            }
+        ]
+    },
+    {
+        "name": "section-title"
+    },
+    {
+        "name": "row",
+        "elems": [
+            {
+                "name": "col",
+                "mods": [
+                    {
+                        "name": "sw",
+                        "vals": [
+                            {
+                                "name": 4
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "row",
+        "elems": [
+            {
+                "name": "col",
+                "mods": [
+                    {
+                        "name": "so",
+                        "vals": [
+                            {
+                                "name": 1
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "quote-card"
+    },
+    {
+        "name": "row",
+        "elems": [
+            {
+                "name": "col",
+                "mods": [
+                    {
+                        "name": "sw",
+                        "vals": [
+                            {
+                                "name": 7
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "link"
+    },
+    {
+        "name": "section",
+        "mods": [
+            {
+                "name": "color",
+                "vals": [
+                    {
+                        "name": "main"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "row",
+        "mods": [
+            {
+                "name": "sac",
+                "vals": [
+                    {
+                        "name": true
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "row",
+        "elems": [
+            {
+                "name": "col",
+                "mods": [
+                    {
+                        "name": "sw",
+                        "vals": [
+                            {
+                                "name": 12
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "section",
+        "elems": [
+            {
+                "name": "title"
+            }
+        ]
+    },
+    {
+        "name": "tabs"
+    },
+    {
+        "name": "tabs",
+        "mods": [
+            {
+                "name": "view",
+                "vals": [
+                    {
+                        "name": "courses"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "row",
+        "elems": [
+            {
+                "name": "col",
+                "mods": [
+                    {
+                        "name": "sw",
+                        "vals": [
+                            {
+                                "name": 3
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "course-card"
+    },
+    {
+        "name": "section",
+        "mods": [
+            {
+                "name": "color",
+                "vals": [
+                    {
+                        "name": "grey"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "slider"
+    },
+    {
+        "name": "slider",
+        "elems": [
+            {
+                "name": "slides"
+            }
+        ]
+    },
+    {
+        "name": "slider",
+        "elems": [
+            {
+                "name": "slide"
+            }
+        ]
+    },
+    {
+        "name": "slider",
+        "elems": [
+            {
+                "name": "slide-content"
+            }
+        ]
+    },
+    {
+        "name": "slider",
+        "elems": [
+            {
+                "name": "person"
+            }
+        ]
+    },
+    {
+        "name": "slider",
+        "elems": [
+            {
+                "name": "content"
+            }
+        ]
+    },
+    {
+        "name": "slider",
+        "elems": [
+            {
+                "name": "footer"
+            }
+        ]
+    },
+    {
+        "name": "slider",
+        "elems": [
+            {
+                "name": "pagination"
+            }
+        ]
+    },
+    {
+        "name": "slider",
+        "elems": [
+            {
+                "name": "next-button"
+            }
+        ]
+    },
+    {
+        "name": "slider",
+        "elems": [
+            {
+                "name": "prev-button"
+            }
+        ]
+    },
+    {
+        "name": "steps"
+    },
+    {
+        "name": "steps",
+        "elems": [
+            {
+                "name": "step"
+            }
+        ]
+    },
+    {
+        "name": "steps",
+        "elems": [
+            {
+                "name": "content"
+            }
+        ]
+    },
+    {
+        "name": "steps",
+        "elems": [
+            {
+                "name": "footer"
+            }
+        ]
+    },
+    {
+        "name": "button",
+        "mods": [
+            {
+                "name": "view",
+                "vals": [
+                    {
+                        "name": "plain"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "button",
+        "mods": [
+            {
+                "name": "size",
+                "vals": [
+                    {
+                        "name": "m"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "button",
+        "mods": [
+            {
+                "name": "disabled",
+                "vals": [
+                    {
+                        "name": true
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "steps",
+        "elems": [
+            {
+                "name": "prev-button"
+            }
+        ]
+    },
+    {
+        "name": "button",
+        "mods": [
+            {
+                "name": "view",
+                "vals": [
+                    {
+                        "name": "main"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "steps",
+        "elems": [
+            {
+                "name": "next-button"
+            }
+        ]
+    },
+    {
+        "name": "form-main"
+    },
+    {
+        "name": "form"
+    },
+    {
+        "name": "form-field"
+    },
+    {
+        "name": "form-field",
+        "mods": [
+            {
+                "name": "type",
+                "vals": [
+                    {
+                        "name": "input"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "form-main",
+        "elems": [
+            {
+                "name": "form-field"
+            }
+        ]
+    },
+    {
+        "name": "input"
+    },
+    {
+        "name": "form-main",
+        "elems": [
+            {
+                "name": "combo"
+            }
+        ]
+    },
+    {
+        "name": "button",
+        "mods": [
+            {
+                "name": "type",
+                "vals": [
+                    {
+                        "name": "submit"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "form-main",
+        "elems": [
+            {
+                "name": "button"
+            }
+        ]
+    },
+    {
+        "name": "section",
+        "mods": [
+            {
+                "name": "type",
+                "vals": [
+                    {
+                        "name": "faq"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "accrodion"
+    },
+    {
+        "name": "section",
+        "mods": [
+            {
+                "name": "color",
+                "vals": [
+                    {
+                        "name": "dark"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "footer"
+    },
+    {
+        "name": "footer",
+        "elems": [
+            {
+                "name": "menu"
+            }
+        ]
+    },
+    {
+        "name": "footer",
+        "elems": [
+            {
+                "name": "menu-item"
+            }
+        ]
+    },
+    {
+        "name": "footer",
+        "elems": [
+            {
+                "name": "copyright"
+            }
+        ]
+    }
+];
---
+++ desktop.bundles/index/index.deps.js
@@ -0,0 +1,741 @@
+exports.deps = [
+    {
+        "block": "page"
+    },
+    {
+        "block": "i-bem",
+        "elem": "dom"
+    },
+    {
+        "block": "jquery"
+    },
+    {
+        "block": "loader"
+    },
+    {
+        "block": "loader",
+        "mod": "type"
+    },
+    {
+        "block": "jquery",
+        "elem": "config"
+    },
+    {
+        "block": "ua"
+    },
+    {
+        "block": "objects"
+    },
+    {
+        "block": "functions"
+    },
+    {
+        "block": "dom"
+    },
+    {
+        "block": "i-bem",
+        "elem": "dom",
+        "mod": "init"
+    },
+    {
+        "block": "i-bem"
+    },
+    {
+        "block": "i-bem",
+        "elem": "internal"
+    },
+    {
+        "block": "inherit"
+    },
+    {
+        "block": "identify"
+    },
+    {
+        "block": "next-tick"
+    },
+    {
+        "block": "events"
+    },
+    {
+        "block": "page",
+        "elem": "css"
+    },
+    {
+        "block": "page",
+        "elem": "js"
+    },
+    {
+        "block": "variables"
+    },
+    {
+        "block": "page",
+        "elem": "meta"
+    },
+    {
+        "block": "section"
+    },
+    {
+        "block": "section",
+        "mod": "type"
+    },
+    {
+        "block": "section",
+        "mod": "type",
+        "val": "main"
+    },
+    {
+        "block": "header"
+    },
+    {
+        "block": "row"
+    },
+    {
+        "block": "header",
+        "elem": "menu-row"
+    },
+    {
+        "block": "sections-menu"
+    },
+    {
+        "block": "menu"
+    },
+    {
+        "block": "control"
+    },
+    {
+        "block": "jquery",
+        "elem": "event"
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type"
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type",
+        "val": "pointerclick"
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type",
+        "val": "pointernative"
+    },
+    {
+        "block": "menu-item"
+    },
+    {
+        "block": "keyboard",
+        "elem": "codes"
+    },
+    {
+        "block": "menu",
+        "mod": "focused"
+    },
+    {
+        "block": "menu",
+        "elem": "group"
+    },
+    {
+        "block": "menu",
+        "elem": "group-title"
+    },
+    {
+        "block": "link"
+    },
+    {
+        "block": "link",
+        "mod": "disabled"
+    },
+    {
+        "block": "header",
+        "elem": "menu"
+    },
+    {
+        "block": "sections-menu",
+        "elem": "menu-item"
+    },
+    {
+        "block": "sections-menu",
+        "elem": "menu-item",
+        "mod": "active"
+    },
+    {
+        "block": "header",
+        "elem": "menu-item"
+    },
+    {
+        "block": "social"
+    },
+    {
+        "block": "logo"
+    },
+    {
+        "block": "image"
+    },
+    {
+        "block": "logo",
+        "elem": "image"
+    },
+    {
+        "block": "header",
+        "elem": "logo"
+    },
+    {
+        "block": "row",
+        "elem": "col"
+    },
+    {
+        "block": "row",
+        "elem": "col",
+        "mod": "sw"
+    },
+    {
+        "block": "title"
+    },
+    {
+        "block": "header",
+        "elem": "title"
+    },
+    {
+        "block": "text"
+    },
+    {
+        "block": "header",
+        "elem": "text"
+    },
+    {
+        "block": "button"
+    },
+    {
+        "block": "button",
+        "mod": "focused"
+    },
+    {
+        "block": "button",
+        "mod": "hovered"
+    },
+    {
+        "block": "button",
+        "mod": "disabled"
+    },
+    {
+        "block": "button",
+        "mod": "pressed"
+    },
+    {
+        "block": "button",
+        "elem": "text"
+    },
+    {
+        "block": "button",
+        "mod": "view"
+    },
+    {
+        "block": "button",
+        "mod": "view",
+        "val": "second"
+    },
+    {
+        "block": "button",
+        "mod": "size"
+    },
+    {
+        "block": "button",
+        "mod": "size",
+        "val": "l"
+    },
+    {
+        "block": "header",
+        "elem": "button"
+    },
+    {
+        "block": "section-title"
+    },
+    {
+        "block": "icon"
+    },
+    {
+        "block": "icon",
+        "mod": "type"
+    },
+    {
+        "block": "icon",
+        "mod": "type",
+        "val": "diplom"
+    },
+    {
+        "block": "icon",
+        "mod": "type",
+        "val": "originality"
+    },
+    {
+        "block": "icon",
+        "mod": "type",
+        "val": "employment"
+    },
+    {
+        "block": "icon",
+        "mod": "type",
+        "val": "how-to"
+    },
+    {
+        "block": "row",
+        "elem": "col",
+        "mod": "sw",
+        "val": 4
+    },
+    {
+        "block": "row",
+        "elem": "col",
+        "mod": "so"
+    },
+    {
+        "block": "quote-card"
+    },
+    {
+        "block": "row",
+        "elem": "col",
+        "mod": "sw",
+        "val": 7
+    },
+    {
+        "block": "section",
+        "mod": "color"
+    },
+    {
+        "block": "section",
+        "mod": "color",
+        "val": "main"
+    },
+    {
+        "block": "row",
+        "mod": "sac"
+    },
+    {
+        "block": "row",
+        "mod": "sac",
+        "val": true
+    },
+    {
+        "block": "row",
+        "elem": "col",
+        "mod": "sw",
+        "val": 12
+    },
+    {
+        "block": "section",
+        "elem": "title"
+    },
+    {
+        "block": "tabs"
+    },
+    {
+        "block": "radio-group"
+    },
+    {
+        "block": "radio"
+    },
+    {
+        "block": "radio",
+        "mod": "disabled"
+    },
+    {
+        "block": "radio",
+        "mod": "checked"
+    },
+    {
+        "block": "radio",
+        "mod": "focused"
+    },
+    {
+        "block": "radio",
+        "elem": "box"
+    },
+    {
+        "block": "radio",
+        "elem": "control"
+    },
+    {
+        "block": "radio",
+        "elem": "text"
+    },
+    {
+        "block": "radio-group",
+        "mod": "theme"
+    },
+    {
+        "block": "radio-group",
+        "mod": "theme",
+        "val": "simple"
+    },
+    {
+        "block": "radio-group",
+        "mod": "type"
+    },
+    {
+        "block": "radio",
+        "mod": "type"
+    },
+    {
+        "block": "button",
+        "mod": "togglable"
+    },
+    {
+        "block": "radio-group",
+        "mod": "type",
+        "val": "line"
+    },
+    {
+        "block": "tabs",
+        "elem": "box"
+    },
+    {
+        "block": "tabs",
+        "elem": "box",
+        "mod": "selected"
+    },
+    {
+        "block": "tabs",
+        "mod": "view"
+    },
+    {
+        "block": "tabs",
+        "mod": "view",
+        "val": "courses"
+    },
+    {
+        "block": "row",
+        "elem": "col",
+        "mod": "sw",
+        "val": 3
+    },
+    {
+        "block": "course-card"
+    },
+    {
+        "block": "section",
+        "mod": "color",
+        "val": "grey"
+    },
+    {
+        "block": "swiper"
+    },
+    {
+        "block": "slider",
+        "elem": "slide"
+    },
+    {
+        "block": "slider",
+        "elem": "footer"
+    },
+    {
+        "block": "slider",
+        "elem": "pagination"
+    },
+    {
+        "block": "slider",
+        "elem": "slides"
+    },
+    {
+        "block": "slider",
+        "elem": "slide-content"
+    },
+    {
+        "block": "slider",
+        "elem": "person"
+    },
+    {
+        "block": "slider",
+        "elem": "content"
+    },
+    {
+        "block": "slider",
+        "elem": "next-button"
+    },
+    {
+        "block": "slider",
+        "elem": "prev-button"
+    },
+    {
+        "block": "steps"
+    },
+    {
+        "block": "steps",
+        "elem": "step"
+    },
+    {
+        "block": "steps",
+        "elem": "content"
+    },
+    {
+        "block": "steps",
+        "elem": "footer"
+    },
+    {
+        "block": "button",
+        "mod": "view",
+        "val": "plain"
+    },
+    {
+        "block": "button",
+        "mod": "size",
+        "val": "m"
+    },
+    {
+        "block": "steps",
+        "elem": "prev-button"
+    },
+    {
+        "block": "button",
+        "mod": "view",
+        "val": "main"
+    },
+    {
+        "block": "steps",
+        "elem": "next-button"
+    },
+    {
+        "block": "form-main"
+    },
+    {
+        "block": "form"
+    },
+    {
+        "block": "form-field"
+    },
+    {
+        "block": "validation"
+    },
+    {
+        "block": "vow"
+    },
+    {
+        "block": "form-field",
+        "mod": "type"
+    },
+    {
+        "block": "form-field",
+        "mod": "type",
+        "val": "input"
+    },
+    {
+        "block": "input"
+    },
+    {
+        "block": "input",
+        "mod": "disabled"
+    },
+    {
+        "block": "input",
+        "mod": "focused"
+    },
+    {
+        "block": "input",
+        "elem": "box"
+    },
+    {
+        "block": "input",
+        "elem": "control"
+    },
+    {
+        "block": "tick"
+    },
+    {
+        "block": "idle"
+    },
+    {
+        "block": "input",
+        "mod": "invalid"
+    },
+    {
+        "block": "input",
+        "mod": "dirty"
+    },
+    {
+        "block": "form-main",
+        "elem": "form-field"
+    },
+    {
+        "block": "form-main",
+        "elem": "combo"
+    },
+    {
+        "block": "button",
+        "mod": "type"
+    },
+    {
+        "block": "button",
+        "mod": "type",
+        "val": "submit"
+    },
+    {
+        "block": "form-main",
+        "elem": "button"
+    },
+    {
+        "block": "section",
+        "mod": "type",
+        "val": "faq"
+    },
+    {
+        "block": "accrodion"
+    },
+    {
+        "block": "section",
+        "mod": "color",
+        "val": "dark"
+    },
+    {
+        "block": "footer"
+    },
+    {
+        "block": "footer",
+        "elem": "menu"
+    },
+    {
+        "block": "footer",
+        "elem": "menu-item"
+    },
+    {
+        "block": "footer",
+        "elem": "copyright"
+    },
+    {
+        "block": "i-bem",
+        "elem": "dom",
+        "mod": "init",
+        "val": "auto"
+    },
+    {
+        "block": "loader",
+        "mod": "type",
+        "val": "js"
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type",
+        "val": "pointer"
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type",
+        "val": "pointerpressrelease"
+    },
+    {
+        "block": "menu",
+        "mod": "focused",
+        "val": true
+    },
+    {
+        "block": "sections-menu",
+        "elem": "menu-item",
+        "mod": "active",
+        "val": true
+    },
+    {
+        "block": "row",
+        "elem": "col",
+        "mod": "sw",
+        "val": 10
+    },
+    {
+        "block": "button",
+        "mod": "focused",
+        "val": true
+    },
+    {
+        "block": "button",
+        "mod": "hovered",
+        "val": true
+    },
+    {
+        "block": "button",
+        "mod": "disabled",
+        "val": true
+    },
+    {
+        "block": "button",
+        "mod": "pressed",
+        "val": true
+    },
+    {
+        "block": "icon",
+        "mod": "type",
+        "val": "about"
+    },
+    {
+        "block": "row",
+        "elem": "col",
+        "mod": "so",
+        "val": 1
+    },
+    {
+        "block": "radio-group",
+        "mod": "theme",
+        "val": "islands"
+    },
+    {
+        "block": "radio",
+        "mod": "disabled",
+        "val": true
+    },
+    {
+        "block": "radio",
+        "mod": "checked",
+        "val": true
+    },
+    {
+        "block": "radio",
+        "mod": "focused",
+        "val": true
+    },
+    {
+        "block": "radio-group",
+        "mod": "type",
+        "val": "button"
+    },
+    {
+        "block": "radio",
+        "mod": "type",
+        "val": "button"
+    },
+    {
+        "block": "button",
+        "mod": "togglable",
+        "val": "radio"
+    },
+    {
+        "block": "tabs",
+        "elem": "box",
+        "mod": "selected",
+        "val": true
+    },
+    {
+        "block": "slider"
+    },
+    {
+        "block": "input",
+        "mod": "invalid",
+        "val": true
+    },
+    {
+        "block": "input",
+        "mod": "disabled",
+        "val": true
+    },
+    {
+        "block": "input",
+        "mod": "focused",
+        "val": true
+    },
+    {
+        "block": "input",
+        "mod": "dirty",
+        "val": true
+    }
+];
